7a6fa45c6a5d735015b8ccfc2a763c9576dd38e8
elhectorlitro <87398353+elhectorlitro@users.noreply.github.com>
2024-09-30 09:05:47 -0600

    Add files via upload

---
+++ Exercise1.c
@@ -0,0 +1,14 @@
+
+int main()
+{
+  char arr[5][10] = {"Bosch", "Bangalore", "Mexico", "Canada", "India"};
+  
+  for (int i = 0; i < 3; i++) 
+  {
+    if (arr[i][1] == 'b' || arr[i][1] == 'b')
+    {
+       printf("%s\n", arr[i]);
+    }
+  }
+  return 0;
+}
